9ff754e29aba62fe6db061682a6c2bfee687c83d
Usama Hassan <96824810+usamahassan965@users.noreply.github.com>
2026-01-08 22:26:09 +0500

    Add files via upload

---
+++ Advanced_Computer_vision/05_Vision_Language_Models_(VLMs)/03_Flamingo/flamingo_pytorch/__init__.py
@@ -0,0 +1,2 @@
+from flamingo_pytorch.flamingo_pytorch import PerceiverResampler, GatedCrossAttentionBlock
+from flamingo_pytorch.flamingo_palm import FlamingoPaLM
---
+++ Advanced_Computer_vision/05_Vision_Language_Models_(VLMs)/03_Flamingo/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'flamingo-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.1.2',
+  license='MIT',
+  description = 'Flamingo - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/flamingo-pytorch',
+  long_description_content_type = 'text/markdown',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'attention mechanism',
+    'visual question answering'
+  ],
+  install_requires=[
+    'einops>=0.4',
+    'einops-exts',
+    'torch>=1.6'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
